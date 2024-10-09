1899fb273267f8632ec8967d5d04f142b0912681
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2024-10-09 16:50:56 +0700

    [Add z index for private nav]

--- src/components/Navbar/PrivateNavbar.jsx
+++ src/components/Navbar/PrivateNavbar.jsx
@@ -129,7 +129,7 @@ const PrivateNavbar = () => {
                   {/* Notification */}
                 </div>
                 <NotificationCounts />
-                <div className='hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center'>
+                <div className='hidden z-20 md:ml-4 md:flex md:flex-shrink-0 md:items-center'>
                   {/* Profile dropdown */}
                   <Menu as='div' className='relative'>
                     <div>
@@ -157,7 +157,7 @@ const PrivateNavbar = () => {
                       leaveFrom='transform opacity-100 scale-100'
                       leaveTo='transform opacity-0 scale-95'
                     >
-                      <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                      <Menu.Items className='absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
                         <Menu.Item>
                           {({ active }) => (
                             <Link
